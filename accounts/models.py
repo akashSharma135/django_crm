998be502b4256475382e8a87ae3dac61618151c5
akashSharma135 <akashsharma1352000@gmail.com>
2021-12-25 15:12:04 +0530

    created accounts app and added templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+        
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=100)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+    Description = models.TextField(max_length=500)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delievery', 'Out for delievery'),
+        ('Delievered', 'Delievered')
+    )
+
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request=request, template_name='accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delieverd = orders.filter(status='Delievered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_orders': total_orders,
+        'delievered': delieverd,
+        'pending': pending
+    }
+
+    return render(request=request, template_name='accounts/dashboard.html', context=context)
 
 def products(request):
-    return render(request=request, template_name='accounts/products.html')
+    products = Product.objects.all()
+    return render(request=request, template_name='accounts/products.html', context={'products': products})
 
-def customer(request):
-    return render(request=request, template_name='accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(pk=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    return render(request=request, template_name='accounts/customer.html', context={'customer': customer, 'orders': orders, 'order_count': order_count})
